87f6f296691f950e91d2f8894a4ba8117e99d631
am25109-YIXIN-SUN <am25109@shibaura-it.ac.jp>
2025-12-09 17:38:11 +0800

    Update hw9-3.c

--- hw9-3.c
+++ hw9-3.c
@@ -1,59 +1,48 @@
 #include <stdio.h>
 
 int main() {
-    int A[3][2];
-    int B[2][3];
-    int C[3][3];
+    double A[2][3];
+    double B[3][2];
+    double C[2][2];
     int i, j, k;
 
-    printf("Enter first matrix (3 x 2) and second matrix (2 x 3)\n");
+    for (i = 0; i < 2; i++)
+        for (j = 0; j < 3; j++)
+            scanf("%lf", &A[i][j]);
 
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 2; j++) {
-            scanf("%d", &A[i][j]);
-        }
-    }
-
-    for (i = 0; i < 2; i++) {
-        for (j = 0; j < 3; j++) {
-            scanf("%d", &B[i][j]);
-        }
-    }
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 2; j++)
+            scanf("%lf", &B[i][j]);
 
     printf("The first matrix you entered is\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 2; j++) {
-            printf("%d ", A[i][j]);
-        }
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 3; j++)
+            printf("%g ", A[i][j]);
         printf("\n");
     }
 
     printf("The second matrix you entered is\n");
-    for (i = 0; i < 2; i++) {
-        for (j = 0; j < 3; j++) {
-            printf("%d ", B[i][j]);
-        }
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 2; j++)
+            printf("%g ", B[i][j]);
         printf("\n");
     }
 
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
             C[i][j] = 0;
-            for (k = 0; k < 2; k++) {
+            for (k = 0; k < 3; k++)
                 C[i][j] += A[i][k] * B[k][j];
-            }
         }
     }
 
-    printf("The multiplication product of matrix A and matrix B :\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            printf("%d ", C[i][j]);
-        }
+    printf("The multiplication product of matrix A and matrix B:\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++)
+            printf("%g ", C[i][j]);
         printf("\n");
     }
 
     return 0;
 }
-
 
