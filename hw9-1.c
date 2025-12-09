b7488272deb3dde719e4ee33ebdd1009caefde3c
am25109-YIXIN-SUN <am25109@shibaura-it.ac.jp>
2025-12-09 17:30:35 +0800

    Update hw9-1.c

--- hw9-1.c
+++ hw9-1.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    double a[2][2];
+    int i, j;
+
+    printf("Enter 2 x 2 matrix\n");
+
+    for(i = 0; i < 2; i++){
+        for(j = 0; j < 2; j++){
+            scanf("%lf", &a[i][j]);
+        }
+    }
+
+    printf("You entered\n");
+    printf("%.0f %.0f\n", a[0][0], a[0][1]);
+    printf("%.0f %.0f\n", a[1][0], a[1][1]);
+
+    double det = a[0][0] * a[1][1] - a[0][1] * a[1][0];
+    printf("Determinant is %f\n", det);
+
+    return 0;
+}
+
 
